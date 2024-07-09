f7d729e577541e8465468342e9855272a8697452
shota24kobashigawa <159881264+shota24kobashigawa@users.noreply.github.com>
2024-07-09 14:37:28 +0900

    Add files via upload

---
+++ kadai_007/associative-array.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html lang="ja">
+ 
+<head>
+     <meta charset="UTF-8">
+     <title>PHP基礎編-課題007</title>
+</head>
+ 
+<body>
+  <p>
+    <?php
+    // 連想配列に値を代入する
+    $merchandise_data = ['name' => 'onion', 'price' => 200, 'weight' => '160'];
+
+    // 連想配列の値を出力する
+    print_r($merchandise_data);
+    ?>
+  </p>
+</body>
+
+</html>
